f7a03604cb9d19d71ab1ae761fe4f0ff43574720
Shinji-Q <endoqueiroz@gmail.com>
2022-11-17 23:20:14 -0300

    remoção de arquivo que não será usado

--- src/components/Card.tsx
+++
@@ -1,9 +0,0 @@
-type argsCard = {
-    text: String;
-}
-
-export function Card(args: argsCard){
-
-    return <h3>{args.text}</h3>
-
-}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,36 +1,14 @@
 import { useState } from 'react';
-import { Card } from "../components/Card";
 import { api } from "../shared/services/api";
 import wasabiLogo from "../assets/wasabi-logo.png";
 import "../../style/Home.css"
 
 
 export function Home(){
-    const [cards, setCards] = useState<String[]>(["um", "dois"]);
 
-    function addCard(){
-
-        var campo = (window.document.querySelector("input#campo") as HTMLInputElement);
-
-        setCards([...cards, campo.value]) ;
-
-        campo.value = "";
-
-    }
     return (
 
     <section id="HomePage" >
-      <section></section>
-      <h1>testetesteTESTE</h1>
-      <img src={wasabiLogo} alt="wasabiLogo" id="wasabiLogo"/>
-      {
-        cards.map(card=>{
-          return <Card text={card} key={3}/>
-        })
-      }
-
-      <input type="text" id="campo"/>
-      <button onClick={addCard}>Botão</button>
 
     </section>
 
