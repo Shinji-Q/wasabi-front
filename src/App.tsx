786fb552f3fcf0d7af4c25a8e7a9c013415fd518
Shinji-Q <endoqueiroz@gmail.com>
2022-11-19 00:41:52 -0300

    "App.tsx" alterado de função para classe

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,33 @@
-import { useState } from "react";
-import { Card } from "./components/Card";
+import React, { useState } from "react";
 import { api } from "./shared/services/api";
 import wasabiLogo from "./assets/wasabi-logo.png";
 import '../style/App.css';
 import { Outlet } from "react-router-dom";
 
 
-export function App() {
-  //imutabilidade
+export class App extends React.Component{
+  constructor(props:object){
+    super(props);
+  }
 
-  return (
-  <div>
-    <div id="content">
-      <Outlet />
-    </div>
-    <section className="topbar" >
-      <section className="menu">
-        <img src={wasabiLogo} alt="wasabiLogo" id="logoMenu"/>
-        <a href={`/`}><span className="bottomText">inicio</span></a>
-        <a href={`cardapio`}><span className="bottomText">cardapio</span></a>
-        <a href={`cart`}><span className="bottomText">carrinho</span></a>
-        <a href={`login`}><span className="bottomText">perfil</span></a>
+  render() {
+    return (
+    <div>
+      <div id="content">
+        <Outlet />
+      </div>
+      <section className="topbar" >
+        <section className="menu">
+          <img src={wasabiLogo} alt="wasabiLogo" id="logoMenu"/>
+          <a href={`/`}><span className="bottomText">inicio</span></a>
+          <a href={`cardapio`}><span className="bottomText">cardapio</span></a>
+          <a href={`cart`}><span className="bottomText">carrinho</span></a>
+          <a href={`login`}><span className="bottomText">perfil</span></a>
+        </section>
       </section>
-    </section>
-  </div>
+    </div>
 
-  );
-
+    );
+  }
 }
 
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { App } from './App';
 import { Cart } from './pages/Cart';
 import { LoginPage } from './pages/LoginPage';
 import { Home } from './pages/Home';
