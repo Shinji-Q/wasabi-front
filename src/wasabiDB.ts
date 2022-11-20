7293ece86ff108a8cececd3a04ed27fc1ce75cd4
Alfredo Gabriel <amgabriel1@hotmail.com>
2022-11-20 19:56:52 -0300

    update: ingração api

--- src/wasabiDB.ts
+++ src/wasabiDB.ts
@@ -78,26 +78,7 @@ export default class WasabiDBApi{
     constructor () {
 
     }
-    static async getCategorias():Promise<categoria[]> {
-        const resp = (await api.get<categoria[]>("/categoria")).data
-        return resp;
-    }
-
-    static async getCategoria(categoriaId:number):Promise<categoria> {
-        const resp = (await api.get<categoria>("/categoria/"+categoriaId)).data;
-        return resp;
-    }
-
-    static async getPratos():Promise<prato[]> {
-        const resp = (await api.get<prato[]>("/produto")).data
-        return resp;
-    }
-
-    static async getPrato(produtoId:number):Promise<prato> {
-        const resp = (await api.get<prato>("/produto/"+produtoId)).data
-        return resp;
-    }
-
+    
     static async getPratosByCat(categoriaId:number):Promise<prato[]> {
         const resp = (await api.get<prato[]>(`produto/cat/${categoriaId}`)).data
         return resp;
@@ -107,59 +88,143 @@ export default class WasabiDBApi{
 
     }
 
+    static async getCategorias():Promise<categoria[]> {
+        const resp = (await api.get<categoria[]>("/categoria")).data
+        return resp;
+    }
+    
+    static async getCategoria(categoriaId:number):Promise<categoria> {
+        const resp = (await api.get<categoria>("/categoria/"+categoriaId)).data;
+        return resp;
+    }
+    
+    static async createCategoria(categoria:categoria):Promise<categoria> {
+        const resp = (await api.post<categoria>("/categoria/", categoria)).data;
+        return resp;
+    }
+    
+    static async updateCategoria(categoria:categoria):Promise<categoria> {
+        const resp = (await api.put<categoria>("/categoria/", categoria)).data;
+        return resp;
+    }
+    
+    static async getPratos():Promise<prato[]> {
+        const resp = (await api.get<prato[]>("/produto")).data
+        return resp;
+    }
+    
+    static async getPrato(produtoId:number):Promise<prato> {
+        const resp = (await api.get<prato>("/produto/"+produtoId)).data
+        return resp;
+    }
+    
+    static async createPrato(prato:prato):Promise<prato> {
+        const resp = (await api.post<prato>("/produto/", prato)).data;
+        return resp;
+    }
+    
+    static async updatePrato(prato:prato):Promise<prato> {
+        const resp = (await api.put<prato>("/produto/", prato)).data;
+        return resp;
+    }
+    
     static async getEnderecos():Promise<endereco[]> {
         const resp = (await api.get<endereco[]>("/endereco/")).data
         return resp;
     }
-
+    
     static async getEndereco(enderecoId:number):Promise<endereco> {
         const resp = (await api.get<endereco>("/endereco/"+enderecoId)).data
         return resp;
     }
-
+    
+    static async createEndereco(endereco:endereco):Promise<endereco> {
+        const resp = (await api.post<endereco>("/endereco/", endereco)).data;
+        return resp;
+    }
+    
+    static async updateEndereco(endereco:endereco):Promise<endereco> {
+        const resp = (await api.put<endereco>("/endereco/", endereco)).data;
+        return resp;
+    }
+    
     static async getCartoes():Promise<cartao[]> {
         const resp = (await api.get<cartao[]>("/cartao/")).data
         return resp;
     }
-
+    
     static async getCartao(cartaoId:number, cartaoCliente:number):Promise<cartao> {
         const resp = (await api.get<cartao>("/cartao/?cartaoId="+ cartaoId +"&cartaoCliente="+ cartaoCliente)).data
         return resp;
     }
-
+    
+    static async createCartao(cartao:cartao):Promise<cartao> {
+        const resp = (await api.post<cartao>("/cartao/", cartao)).data;
+        return resp;
+    }
+    
+    static async updateCartao(cartao:cartao):Promise<cartao> {
+        const resp = (await api.put<cartao>("/cartao/", cartao)).data;
+        return resp;
+    }
+    
     static async getClientes():Promise<cliente[]> {
         const resp = (await api.get<cliente[]>("/cliente/")).data
         return resp;
     }
-
+    
     static async getCliente(clienteId:number):Promise<cliente> {
         const resp = (await api.get<cliente>("/cliente/"+clienteId)).data
         return resp;
     }
-
-
+    
+    static async createCliente(cliente:cliente):Promise<cliente> {
+        const resp = (await api.post<cliente>("/cliente/", cliente)).data;
+        return resp;
+    }
+    
+    static async updateCliente(cliente:cliente):Promise<cliente> {
+        const resp = (await api.put<cliente>("/cliente/", cliente)).data;
+        return resp;
+    }
+    
     static async getVendas():Promise<venda[]> {
         const resp = (await api.get<venda[]>("/venda/")).data
         return resp;
     }
-
+    
     static async getVenda(vendaId:number):Promise<venda> {
         const resp = (await api.get<venda>("/venda/"+vendaId)).data
         return resp;
     }
-
+    
+    static async createVenda(venda:venda):Promise<venda> {
+        const resp = (await api.post<venda>("/venda/", venda)).data;
+        return resp;
+    }
+    
+    static async updateVenda(venda:venda):Promise<venda> {
+        const resp = (await api.put<venda>("/venda/", venda)).data;
+        return resp;
+    }
+    
     static async getUsuarios():Promise<usuario[]> {
         const resp = (await api.get<usuario[]>("/usuario/")).data
         return resp;
     }
-
+    
     static async getUsuario(usuarioId:number):Promise<usuario> {
         const resp = (await api.get<usuario>("/usuario/"+usuarioId)).data
         return resp;
     }
-
-    static async postVenda(venda:venda):Promise<venda> {
-        const resp = (await api.post("/venda", venda)).data;
+    
+    static async createUsuario(usuario:usuario):Promise<usuario> {
+        const resp = (await api.post<usuario>("/usuario/", usuario)).data;
+        return resp;
+    }
+    
+    static async updateUsuario(usuario:usuario):Promise<usuario> {
+        const resp = (await api.put<usuario>("/usuario/", usuario)).data;
         return resp;
     }
     
