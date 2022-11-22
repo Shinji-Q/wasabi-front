180d2eb64d5b759fe49eb3a30c35d9715e35b40c
Shinji-Q <endoqueiroz@gmail.com>
2022-11-22 11:37:34 -0300

    tweaks em pratoCard

--- src/pages/Cardapio/PratoCard.tsx
+++ src/pages/Cardapio/PratoCard.tsx
@@ -11,7 +11,8 @@ type PratoCardProps = {
 
 
 export function PratoCard(props:PratoCardProps) {
-    const { produtoId } = props.prato
+    const { prato } = props
+    const { produtoId } = prato
 
     const [inCart, setInCart] = useState(Cookies.sacola.get(produtoId.toString())!==undefined);
     const [quantidade, setQuantidade] = useState(Cookies.sacola.get(produtoId.toString())??0);
@@ -67,40 +68,47 @@ export function PratoCard(props:PratoCardProps) {
     return(
         <div className="pratoCard">
             <div id="descricao">
-                <div className="descTextoCard">
-                    <p id="nomePrato">
-                        {props.prato.produtoNome}
+                <div className="desc">
+                    <div className="descTextoCard">
+                    <h1 className="nomePrato">
+                        {prato.produtoNome}
+                    </h1>
+                    <p className="descricaoPrato">
+                        {prato.produtoDescricao}
                     </p>
-                    <p>
-                        {props.prato.produtoDescricao}
-                    </p>
+                    </div>
+                    <div className="descFooter" >
+                        <p className="preco">
+                            {prato.produtoPreco.toLocaleString('pt-BR', {style:'currency',currency:'BRL'})}
+                        </p>
+                        <div className="acoes">
+                            {!inCart &&
+                                <button id="add" onClick={adicionarAoCarrinho}>Adicionar à sacola</button>
+                            }
+                            {inCart &&
+                            <div className="cartProdControler">
+                                <button className="botaoDeAcao" id="removerUm" onClick={(ev) => {remUm()}}>
+                                    <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+                                        <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+                                    </svg>
+                                </button>
+                                
+                                <input className="mx-2 border text-center w-8" type="number"  onChange={(ev)=> {alterarQuantidade(ev)}} value={quantidade}/>
+
+                                <button className="botaoDeAcao" id="adicionar" onClick={(ev) => {addUm()}}>
+                                    <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+                                        <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                            }
+                        </div>
+                    </div>
                 </div>
                 <img className="imagemPratoCard"src={`data:image/png;base64,${props.prato.produtoImagem??""}` }/>
                 {/* @ts-ignore */}
             </div>
             {/*@ts-ignore  */}
-            <div id="acoes" >
-                {!inCart &&
-                    <button id="add" onClick={adicionarAoCarrinho}>Adicionar à sacola</button>
-                }
-                {inCart &&
-                <>
-                    <button className="botaoDeAcao" id="removerUm" onClick={(ev) => {remUm()}}>
-                        <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-                            <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-                        </svg>
-                    </button>
-                    
-                    <input className="mx-2 border text-center w-8" type="number"  onChange={(ev)=> {alterarQuantidade(ev)}} value={quantidade}/>
-
-                    <button className="botaoDeAcao" id="adicionar" onClick={(ev) => {addUm()}}>
-                        <svg className="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-                            <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-                        </svg>
-                    </button>
-                </>
-                }
-            </div>
         </div>
     )
 }
